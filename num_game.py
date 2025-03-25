091af914ad5460c57cf91f4eaa6bd23603eaf596
Module_0_2 <Lisasve546@gmail.com>
2025-03-25 21:39:55 +0700

    Числовая угадайка

--- num_game.py
+++ num_game.py
@@ -1,29 +1,42 @@
 import random
-hidden_number = random.randint(1, 100)
-print('Добро пожаловать в числовую угадайку')
-def is_valid(n):
-    if 1 <= int(n) <= 100:
-        return True
-    else:
-        return False
-Flag = True
-count_attempt = 0
-while Flag:
-    n = int(input("Введите число от 1 до 100: "))
-    if not is_valid(n):
-        print("А может быть все-таки введем целое число от 1 до 100?")
-        count_attempt += 1
-    elif n < hidden_number:
-        print('Ваше число меньше загаданного, попробуйте еще разок')
-        count_attempt += 1
-    elif n > hidden_number:
-        print('Ваше число больше загаданного, попробуйте еще разок')
-        count_attempt += 1
-    elif n == hidden_number:
-        print('Вы угадали, поздравляем!')
-        print(f'Было использовано {count_attempt} попыток')
-        print('Спасибо, что играли в числовую угадайку. Еще увидимся...')
-        again = input('Сыграем еще раз? да/нет ')
-        Flag = False
 
-print(is_valid(n))
+
+def is_valid(n):
+    """Проверяет, лежит ли число n в диапазоне от 1 до 100."""
+    return 1 <= int(n) <= 100
+
+
+def play_game():
+    # Генерация нового случайного числа
+    hidden_number = random.randint(1, 100)
+    print('Добро пожаловать в числовую угадайку')
+
+    count_attempt = 0
+    while True:
+        n = int(input("Введите число от 1 до 100: "))
+
+        if not is_valid(n):
+            print("А может быть все-таки введем целое число от 1 до 100?")
+            continue  # Начинаем новый виток цикла без увеличения счетчика попыток
+
+        count_attempt += 1
+
+        if n < hidden_number:
+            print('Ваше число меньше загаданного, попробуйте еще разок')
+        elif n > hidden_number:
+            print('Ваше число больше загаданного, попробуйте еще разок')
+        elif n == hidden_number:
+            print('Вы угадали, поздравляем!')
+            print(f'Было использовано {count_attempt} попыток')
+            break  # Выходим из текущего цикла игры
+
+    print('Спасибо, что играли в числовую угадайку. Еще увидимся...')
+
+
+# Основной игровой цикл
+while True:
+    play_game()
+    again = input('Сыграем еще раз? да/нет ').lower().strip()  # Приводим строку к нижнему регистру
+    if again != 'да':
+        print('До новых встреч!')
+        break
